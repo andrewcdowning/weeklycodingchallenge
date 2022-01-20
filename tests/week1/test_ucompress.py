8d3dd5c8f33693fe2d256d5214599e95b2c83218
Andrew Downing <andrew.c.downing@gmail.com>
2022-01-19 20:35:02 -0700

    test: updating test cases for function

--- tests/week1/test_ucompress.py
+++ tests/week1/test_ucompress.py
@@ -1,13 +1,16 @@
 from weeklycodingchallenge.week1 import uncompress
+import pytest
 
-
+@pytest.mark.parametrize("test_input,expected", [
+    ("127y", "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"),
+     ("2p1o5p", "ppoppppp"), 
+     ("3n12e2z", "nnneeeeeeeeeeeezz")])  # noqa
 def test_uncompress():
     """ Test the uncompress function """
 
     # Given
-    input = "127y"
-    expected = "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"  # noqa
+    """ test_input and expected """
     # When
-    result = uncompress(input)
+    result = uncompress(test_input)
     # Then
     assert result == expected
